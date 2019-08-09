6be8ec5975803942870d216661b563e1ed6c5319
luza13 <50076797+luza13@users.noreply.github.com>
2019-08-08 20:08:52 -0500

    Profesor no entiendo

---
+++ PROYECTO FORMULARIOS.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Usuario
+ * Fecha: 5/06/2019
+ * Hora: 6:31 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LicambaLuzBernal
+{
+	/// <summary>
+	/// Description of FormMenu.
+	/// </summary>
+	public partial class FormMenu : Form
+	{
+		public FormMenu()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void FormMenuLoad(object sender, EventArgs e)
+		{
+	
+		}
+	}
+}
